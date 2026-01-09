f19fab014c6071b538f9aecfd60df20ad25fff51
Raphaël MANSUY <raphael.mansuy@gmail.com>
2026-01-09 14:12:45 +0800

    docs: Add FEAT/BR refs to WebUI providers (OODA-45)

--- edgequake_webui/src/providers/theme-provider.tsx
+++ edgequake_webui/src/providers/theme-provider.tsx
@@ -1,3 +1,12 @@
+/**
+ * @module ThemeProvider
+ * @description Theme context provider using next-themes.
+ *
+ * @implements FEAT0800 - Theme support (light/dark/system)
+ *
+ * @enforces BR0800 - Theme persisted in localStorage
+ * @enforces BR0862 - No transition flash on theme change
+ */
 'use client';
 
 import type { ThemeProviderProps } from 'next-themes';
